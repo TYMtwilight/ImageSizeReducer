9cb46be19d7dd5c8ab4a46d83f56b287663b5115
TYMtwilight <ore01100001binary@gmail.com>
2022-06-05 23:25:14 +0900

    ✨ CanvasイメージをJPEGデータとしてダウンロードする機能を実装

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const fileZone = document.querySelector(".file-zone");
 const on = "on";
+const filenames = [];
+
 fileZone.addEventListener("dragover", (event) => {
   event.preventDefault();
   fileZone.classList.add(on);
 });
+
 fileZone.addEventListener("dragleave", (event) => {
   event.preventDefault();
   fileZone.classList.remove(on);
 });
+
 fileZone.addEventListener("drop", (event) => {
   event.preventDefault();
   fileZone.classList.remove(on);
@@ -28,6 +32,7 @@ fileZone.addEventListener("drop", (event) => {
 const displayImage = (imageList) => {
   const imageBox = document.querySelector(".image-box");
   imageList.forEach((imageFile) => {
+    filenames.push(imageFile.name);
     const reader = new FileReader();
     reader.readAsDataURL(imageFile);
     reader.addEventListener("load", (event) => {
@@ -35,6 +40,7 @@ const displayImage = (imageList) => {
       image.src = event.target.result;
       image.onload = () => {
         const canvas = document.createElement("canvas");
+        canvas.classList.add(".canvas-image");
         const MAX_WIDTH = 400;
         const IMG_WIDTH = image.naturalWidth;
         const scaleSize = MAX_WIDTH / IMG_WIDTH;
@@ -47,7 +53,6 @@ const displayImage = (imageList) => {
     });
   });
   const canvasNum = imageList.length;
-  console.log(canvasNum);
   if (canvasNum % 5 !== 0) {
     const shortage = 5 - (canvasNum % 5);
     console.log(shortage);
@@ -56,4 +61,28 @@ const displayImage = (imageList) => {
       imageBox.insertBefore(empty, imageBox.lastChild);
     }
   }
+  console.log(filenames);
 };
+
+const download = document.querySelector(".download");
+download.addEventListener("click", (event) => {
+  event.preventDefault();
+  const canvasImages = document.getElementsByClassName(".canvas-image");
+  const length = canvasImages.length;
+  for (let i = 0; i < length; i++) {
+    canvasImages[i].toBlob((blob) => {
+      if (window.navigator.msSaveBlob) {
+        window.navigator.msSaveBlob(blob, filenames[i]);
+      } else if (window.URL && window.URL.createObjectURL) {
+        const a = document.createElement("a");
+        a.download = filenames[i];
+        a.href = window.URL.createObjectURL(blob);
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      } else {
+        window.open(base64, "_bloank");
+      }
+    }, "image/jpeg");
+  }
+});
